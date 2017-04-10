0886c6a8ad378bf1fa816ef366b6e8be36fea35d
Rinatius <rinat@kloop.kg>
2017-04-10 19:38:53 +0600

    Create textual interface

--- app/src/main/java/kg/kloop/rinat/hangman/MainActivity.java
+++ app/src/main/java/kg/kloop/rinat/hangman/MainActivity.java
@@ -2,12 +2,32 @@ package kg.kloop.rinat.hangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView gallowsView;
+    private TextView wordView;
+    private TextView lettersView;
+    private EditText guessView;
+    private Button submitButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        gallowsView = (TextView)findViewById(R.id.textViewGallows);
+        wordView = (TextView)findViewById(R.id.textViewWord);
+        lettersView = (TextView)findViewById(R.id.textViewLetters);
+        guessView = (EditText)findViewById(R.id.editTextGuess);
+        submitButton = (Button)findViewById(R.id.buttonSubmit);
     }
 }
