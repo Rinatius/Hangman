c4e974facac104455055dab6edb33cbaceb722de
Rinatius <rinat@kloop.kg>
2017-04-11 19:31:56 +0600

    Implement controller logic

--- app/src/main/java/kg/kloop/rinat/hangman/HangManController.java
+++ app/src/main/java/kg/kloop/rinat/hangman/HangManController.java
@@ -17,13 +17,27 @@ class HangManController {
         model.setWord(word);
     }
 
-    public void acceptGuess(String letter) {
+    public void acceptGuess(char letter) {
         if (model.getWord().contains(letter)){
             showLetter(letter);
         } else model.setGallowPart(model.getGallowPart() + 1);
     }
 
-    private void showLetter(String letter) {
+    private void showLetter(char letter) {
+        model.setRiddle(findLetters(letter, model.getRiddle(),
+                        model.getWord()));
+    }
 
+    private String findLetters (char letter, String riddle, String word){
+        String updatedRiddle = "";
+        char[] riddleArray = riddle.toCharArray();
+        char[] wordArray = word.toCharArray();
+        int i = 0;
+        for (char symbol : wordArray) {
+            if (symbol == letter) riddleArray[i] = letter;
+            i++;
+        }
+        updatedRiddle = new String(riddleArray);
+        return updatedRiddle;
     }
 }
